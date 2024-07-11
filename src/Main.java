d4d201b153161af65eed42b7ceaf6a792c7ffd0b
tranvulong <vulongub2001@gmail.com>
2024-07-11 11:34:51 +0900

    completed project

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Stream;
 
+//Sort을 위해 subClass 생성
+class Sortbydate implements Comparator<Todo> {
+    @Override
+    public int compare(Todo t1, Todo t2) {
+        return t1.getDate().compareTo(t2.getDate());
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
@@ -11,61 +18,79 @@ public class Main {
             System.out.println("Welcome");
             Scanner scan = new Scanner(System.in);
             List<Todo> todoList = new ArrayList<>();
-            todoList.add(new Todo());
             //파일에서 있는 데이터를 다시 todoList에 작성한다
             todoList = bufferedReader(todoList);
+            sort(todoList);
             //제목들을 갖는 todoTitles를 만들다
             List<String> todoTitles = new ArrayList<>();
+            for (Todo t : todoList) {
+                todoTitles.add(t.getTitle());
+            }
+            System.out.println();
+            checkItemCount(todoTitles);
             int index = 1;
             for (Todo t : todoList) {
-                System.out.print(index +": ");
-                todoTitles.add(t.getTitle());
+                System.out.print(index + ": ");
                 System.out.println(t.getTitle());
                 index++;
             }
-
-            System.out.println();
-            checkItemCount(todoTitles);
             outputChoice();
 
             //사용자는 choice(create,edit,finish,delete,exit) 를 선택
-            int choice = scan.nextInt();
+            //Exception 처리
+            int choice;
+            try {
+                choice = Integer.parseInt(scan.next());
+            } catch (NumberFormatException a) {
+                System.out.println("input is not number");
+                break;
+            }
             if (choice == 1) {
+                //Todo클래스의 create 메소드를 사용함 -> 새로운 Todo클래스의 instance를 만들다
                 Todo newTodo = new Todo().create();
+                // 그 instance를 todoList에 추가함
                 todoList.add(newTodo);
-                bufferedWriter(todoList);
+                // todoList을 정렬한다
                 sort(todoList);
+                //todoList의 elements를 파일에 작성한다
+                bufferedWriter(todoList);
             } else if (choice == 2) {
-                todoList.add(new Todo().edit(todoList));
-                bufferedWriter(todoList);
+                //Todo클래스의 edit 메소드를 사용함
+                new Todo().edit(todoList);
                 sort(todoList);
+                bufferedWriter(todoList);
             } else if (choice == 3) {
-                new Todo().finish(todoTitles);
+                //Todo클래스의 finish 메소드를 사용함
+                new Todo().finish(todoList);
+                bufferedWriter(todoList);
             } else if (choice == 4) {
                 System.out.print("Delete TODO number: ");
-                int deleteNum = scan.nextInt();
+                int deleteNum = scan.nextInt() - 1;
                 todoList.remove(deleteNum);
-                bufferedWriter(todoList);
                 sort(todoList);
+                bufferedWriter(todoList);
             } else if (choice == 5) {
                 break;
             }
         }
+        System.out.println();
     }
 
+    // (done)완료된 elelement의 수를 검사하는 메소드
     public static void checkItemCount(List<String> titleList) {
         int count = 0;
         for (String title : titleList) {
-            if (title.contains("done")) {
+            if (title == null || title.contains("done")) {
                 continue;
             }
             count++;
         }
-        System.out.printf("You have %d TODOs leftd", count);
+        System.out.printf("You have %d TODOs left \n", count);
     }
 
+    // 각 선택들을 출력한다
     public static void outputChoice() {
-        System.out.println("1. Create TODO");
+        System.out.println("\n1. Create TODO");
         System.out.println("2. Edit TODO");
         System.out.println("3. Finish TODO");
         System.out.println("4. Delete TODO");
@@ -73,10 +98,11 @@ public class Main {
         System.out.print("\n Input: ");
     }
 
+    //파일에서 있는 데이터를 todoList에 작성함
     public static List<Todo> bufferedReader(List<Todo> todoList) {
         try (FileReader fileReader = new FileReader("todoExport.txt");
              BufferedReader reader = new BufferedReader(fileReader)) {
-            String line ;
+            String line;
             while ((line = reader.readLine()) != null) {
                 String[] elements = line.split(",");
                 todoList.add(new Todo(elements[0], elements[1]));
@@ -88,6 +114,7 @@ public class Main {
         return todoList;
     }
 
+    // todoList에 있는 데이터를 모두 파일을 전달해주는 bufferedWriter
     public static void bufferedWriter(List<Todo> todoList) {
         try (FileWriter fileWriter = new FileWriter(("todoExport.txt"));
              BufferedWriter writer = new BufferedWriter(fileWriter)) {
@@ -95,6 +122,7 @@ public class Main {
                 String line = new StringBuilder().append(todo.getTitle()).append(",")
                         .append(todo.getDate()).toString();
                 writer.write(line);
+                writer.newLine();
             }
         } catch (IOException e) {
             System.out.println("데이터 작성을 위한 파일을 열지 못한다...");
@@ -102,14 +130,9 @@ public class Main {
         }
     }
 
+    //SortbyDate SubClass을 바탕으로 sort method를 만들다
     public static void sort(List<Todo> todoList) {
-        Collections.sort(todoList,new sortbyname());
-    }
-}
-class sortbyname implements Comparator<Todo> {
-    @Override
-    public int compare(Todo t1, Todo t2) {
-        return t1.getDate().compareTo(t2.getDate());
+        Collections.sort(todoList, new Sortbydate());
     }
 }
 
--- src/Todo.java
+++ src/Todo.java
@@ -1,2 +1,75 @@
-package PACKAGE_NAME;public class Todo {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class Todo {
+    private String title;
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Todo(String title, String date) {
+        this.title = title;
+        this.date = date;
+    }
+    public Todo() {
+
+    }
+    Todo create(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Title: ");
+        String title = scan.nextLine();
+        System.out.print("Until: ");
+        String date = scan.nextLine();
+        System.out.println("Saved!!");
+        return new Todo(title,date);
+    }
+    void edit(List<Todo> todoList){
+        //editNumber를 선택한 후 todoList에서 editNumber에 해당한 Todo element를 꺼낸다
+        System.out.print("Edit TODO number: ");
+        Scanner scan = new Scanner(System.in);
+        int editNumber = scan.nextInt() - 1;
+        Todo editTodo = todoList.get(editNumber);
+        //새로운 제목을 받음
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("Title: ");
+        String title = scan1.nextLine();
+        //새론운 date을 받음
+        Scanner scan2 = new Scanner(System.in);
+        System.out.print("Until: ");
+        String date = scan2.nextLine();
+        System.out.println("Saved!!");
+
+        if (title == null) {
+            title = editTodo.getTitle();
+        }
+        if (date == null) {
+            date = editTodo.getTitle();
+        }
+        todoList.remove(editNumber);
+        todoList.add(new Todo(title,date));
+    }
+    void finish(List<Todo> todoList){
+        System.out.print("Finish TODO number: ");
+        Scanner scan = new Scanner(System.in);
+        int finishNumber = scan.nextInt() - 1;
+        String todoTitle = todoList.get(finishNumber).getTitle() + "(done)";
+        String date = todoList.get(finishNumber).getDate();
+        todoList.set(finishNumber,new Todo(todoTitle,date));
+    }
+
 }
